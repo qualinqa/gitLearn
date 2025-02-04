b499d6583ec3dcaf286e0d9bf5fe87582f480b74
irina <i.ivanova@mapcard.pro>
2025-02-04 09:53:08 +0300

    updated

--- src/pages/yourfeedPage.js
+++ src/pages/yourfeedPage.js
@@ -1,7 +1,7 @@
 export class YourfeedPage {
     constructor(page) {
         this.page = page;
-        this.newArticleButton = page.getByRole('link', {name: 'New Article'});
+        this.newArticleButton = page.getByRole('button', {name: 'New Article'});
         this.profileNameField = page.getByRole('navigation');
     }
 
